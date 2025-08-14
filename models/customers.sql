0b4389ae8b63e32feaf6635a48b7d534d379ab35
Muhammad Rasyid Nurrohim <rasyidmuhammad902@gmail.com>
2025-08-14 08:00:57 +0000

    add model, test and docs

--- models/customers.sql
+++ models/customers.sql
@@ -6,22 +6,17 @@
 
 
 with customers as (
-    select
-        id as customer_id,
-        first_name,
-        last_name
 
-    from `dbt-tutorial`.jaffle_shop.customers
+    select * from {{ ref('stg_customers') }}
+
 ),
 
 orders as (
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date
 
-    from `dbt-tutorial`.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
+
 ),
+
 
 customers_orders as (
     select
